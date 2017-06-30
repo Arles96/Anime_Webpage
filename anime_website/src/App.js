b8aaac4077c9fee458e9026b97a0c8b8bf9b4faf
Arles96 <arles.cerrato@gmail.com>
2017-06-29 21:42:02 -0600

    Agregando el header de la pagina

--- anime_website/src/App.js
+++ anime_website/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './img/logo.jpg';
 
 class App extends Component {
 
@@ -10,7 +11,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header className="head-page">
+          <img  id="logo" src={logo} alt="Logo" />
+          <h1>AnimeHN</h1>
+        </header>
       </div>
     );
   }
