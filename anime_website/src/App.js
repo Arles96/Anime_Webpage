59a442276b5bfdce21b50b210ee5c56b43617637
Arles96 <arles.cerrato@gmail.com>
2017-06-30 15:49:40 -0600

    cambios del diseño de la pagina web y finalizacion del contenido de inicio

--- anime_website/src/App.js
+++ anime_website/src/App.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './img/logo.jpg';
+import Home from './Components/Home.js'
 
 class App extends Component {
 
   constructor(){
     super();
+    this.state = {
+      home: true,
+      bingo: false,
+      millionaire: false,
+      animes: false
+    }
+    this.handleContent = this.handleContent.bind(this);
+    this.handleClickHome = this.handleClickHome.bind(this);
+    this.handleClickBingo = this.handleClickBingo.bind(this);
+    this.handleClickMillonaire = this.handleClickMillonaire.bind(this);
+    this.handleClickAnimes = this.handleClickAnimes.bind(this);
+  }
+
+  handleContent(){
+    if (this.state.home) {
+      return (<Home/>);
+    }else if (this.state.bingo) {
+      return(<p>Hola Bingo</p>);
+    }else if (this.state.millionaire) {
+      return (<p>Hola millonario</p>);
+    }else if (this.state.animes) {
+      return(<p>Hola animes</p>);
+    }
+  }
+
+  handleClickHome(){
+    this.setState({
+      home: true,
+      bingo: false,
+      millionaire: false,
+      animes: false
+    });
+  }
+
+  handleClickBingo(){
+    this.setState({
+      home: false,
+      bingo: true,
+      millionaire: false,
+      animes: false
+    });
+  }
+
+  handleClickMillonaire(){
+    this.setState({
+      home: false,
+      bingo: false,
+      millionaire: true,
+      animes: false
+    });
+  }
+
+  handleClickAnimes(){
+    this.setState({
+      home: false,
+      bingo: false,
+      millionaire: false,
+      animes: true
+    });
   }
 
   render() {
@@ -15,6 +75,15 @@ class App extends Component {
           <img  id="logo" src={logo} alt="Logo" />
           <h1>AnimeHN</h1>
         </header>
+        <nav className="Nav-Menu">
+          <button className="button-menu" onClick={this.handleClickHome} >Inicio</button>
+          <button className="button-menu" onClick={this.handleClickBingo} >Bingo</button>
+          <button className="button-menu" onClick={this.handleClickMillonaire} >Quien Quiere ser Millonario</button>
+          <button className="button-menu" onClick={this.handleClickAnimes} >Animes</button>
+        </nav>
+        <section className="content">
+          {this.handleContent()}
+        </section>
       </div>
     );
   }
---
+++ anime_website/src/Components/Home.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component {
+
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <article id="About-us">
+        <h2 className="text-center">
+          ¿Quienes somos?
+        </h2>
+        <br/>
+        <p className="text-justify">
+          Bienvenido a AnimeHN, la presente pagina esta hecha por fines didacticos
+          y no comerciales. Hecho por alumnos de la universidad Tecnologica de
+          Honduras (UNITEC) por los ingenieros Arles Cerrato y Jorge Alvares.
+        </p>
+        <br/>
+        <p className="text-justify">
+          Esta página presentará dos juegos e información de los animes mas
+          populares del mundo. Los juegos que presenta son: Bingo y Quien quiere
+          quiere ser millonario.
+        </p>
+        <br/>
+        <p className="text-center">
+          Esperamos que disfruten la pagina.
+        </p>
+      </article>
+    );
+  }
+
+}
